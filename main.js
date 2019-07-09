b5e4dd496e21cbcd3da8f72cc16c7326197a4b3f
Krzysiek 'Shlizer' Hinc <hinc.krzysiek@gmail.com>
2019-07-09 18:48:28 +0200

    window srv

--- main.js
+++ main.js
@@ -1,17 +1,19 @@
 "use strict";
 
 // Import parts of electron to use
-const { app, ipcMain, shell, BrowserWindow } = require("electron");
+const { app, ipcMain, shell, BrowserWindow, Menu } = require("electron");
 const path = require("path");
-const url = require("url");
+const { createWindow } = require("./srv/window");
 const { parseUrl } = require("./srv/feed");
 const { setOptions, getOptions } = require("./srv/options");
+
+const APP_OPTIONS = getOptions();
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
 
-let DEV =
+const DEV =
   process.defaultApp ||
   /[\\/]electron-prebuilt[\\/]/.test(process.execPath) ||
   /[\\/]electron[\\/]/.test(process.execPath);
@@ -23,60 +25,11 @@ if (process.platform === "win32") {
   app.commandLine.appendSwitch("force-device-scale-factor", "1");
 }
 
-function createWindow() {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    minWidth: 300,
-    minHeight: 40,
-    //frame: false,
-    show: false,
-    webPreferences: {
-      nodeIntegration: true
-    }
-  });
-  mainWindow.setMenu(null);
-
-  // and load the index.html of the app.
-  let indexPath;
-
-  if (DEV && process.argv.indexOf("--noDevServer") === -1) {
-    indexPath = url.format({
-      protocol: "http:",
-      host: "localhost:8080",
-      pathname: "index.html",
-      slashes: true
-    });
-  } else {
-    indexPath = url.format({
-      protocol: "file:",
-      pathname: path.join(__dirname, "dist", "index.html"),
-      slashes: true
-    });
-  }
-
-  mainWindow.loadURL(indexPath);
-
-  // Don't show until we are ready and loaded
-  mainWindow.once("ready-to-show", () => {
-    mainWindow.show();
-
-    // Open the DevTools automatically if developing
-    if (DEV) {
-      mainWindow.webContents.openDevTools();
-    }
-  });
-
-  mainWindow.on("closed", function() {
-    mainWindow = null;
-  });
-}
-
-app.on("ready", createWindow);
+app.on("ready", () => createWindow(APP_OPTIONS, DEV));
 
 // Quit when all windows are closed.
 app.on("window-all-closed", () => {
+  setOptions(APP_OPTIONS);
   if (process.platform !== "darwin") {
     app.quit();
   }
@@ -86,7 +39,7 @@ app.on("activate", () => {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
-    createWindow();
+    createWindow(APP_OPTIONS);
   }
 });
 
@@ -100,7 +53,7 @@ ipcMain.on("initFeeder", (event, urls) => {
  * Options
  */
 ipcMain.on("getOptions", event => {
-  event.returnValue = getOptions();
+  event.returnValue = APP_OPTIONS;
 });
 
 ipcMain.on("setOptions", (event, options) => {
---
+++ srv/window.js
@@ -0,0 +1,67 @@
+const url = require("url");
+const { app, ipcMain, shell, BrowserWindow, Menu } = require("electron");
+
+const createWindow = (OPTIONS, devMode) => {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({
+    title: 'RSS Feeder',
+    width: OPTIONS.window.size.width,
+    height: OPTIONS.window.size.height,
+    minWidth: 300,
+    minHeight: 40,
+    alwaysOnTop: OPTIONS.window.pinned,
+    frame: false,
+    show: false,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+  mainWindow.setPosition(OPTIONS.window.position.top,OPTIONS.window.position.left);
+
+  // and load the index.html of the app.
+  let indexPath;
+
+  if (devMode && process.argv.indexOf("--noDevServer") === -1) {
+    indexPath = url.format({
+      protocol: "http:",
+      host: "localhost:8080",
+      pathname: "index.html",
+      slashes: true
+    });
+  } else {
+    indexPath = url.format({
+      protocol: "file:",
+      pathname: path.join(__dirname, "dist", "index.html"),
+      slashes: true
+    });
+  }
+
+  mainWindow.loadURL(indexPath);
+
+  // Don't show until we are ready and loaded
+  mainWindow.once("ready-to-show", () => {
+    mainWindow.setMenu(Menu.buildFromTemplate([]));
+    mainWindow.setTitle('RSS Feeder');
+    mainWindow.show();
+
+    // Open the DevTools automatically if developing
+    if (devMode) {
+      mainWindow.webContents.openDevTools();
+    }
+  });
+
+  mainWindow.on("closed", function() {
+    mainWindow = null;
+  });
+
+  mainWindow.on("close", function() {
+    OPTIONS.window.position.top = mainWindow.getPosition()[0];
+    OPTIONS.window.position.left = mainWindow.getPosition()[1];
+    OPTIONS.window.size.width = mainWindow.getSize()[0];
+    OPTIONS.window.size.height = mainWindow.getSize()[1];
+    OPTIONS.window.pinned = mainWindow.isAlwaysOnTop();
+    OPTIONS.window.maximized = mainWindow.isMaximized();
+  });
+};
+
+module.exports = { createWindow };
